389c07154ad058c6444c385d8b8f2aed82f80041
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2024-03-10 13:47:02 +0900

    Created an array2D

---
+++ ChekerBoardTest/ChekerBoardTest/main.swift
@@ -0,0 +1,30 @@
+//
+//  main.swift
+//  ChekerBoardTest
+//
+//  Created by Khusrav Safiev on 3/10/24.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+func array2D (rows: Int, colums:Int) -> [[String]] {
+    
+    var strArray = [[String]] ()
+    
+    for _ in 0..<rows {
+        var  row = [String] ()
+        
+        for _ in 0..<colums {
+            row.append("#")
+            print(row)
+        }
+        strArray.append(row)
+    }
+    return strArray
+}
+
+var chekerBoard = array2D(rows: 8, colums: 8)
+print(chekerBoard)
+
